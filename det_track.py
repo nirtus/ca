8e1cd6875d8047743e5cf94d0a15a29c2abe62d0
nirtus <nirtulys@gmail.com>
2023-03-25 04:56:44 +0200

    tracks visualisations

--- det_track.py
+++ det_track.py
@@ -1,25 +1,22 @@
 from ultralytics import YOLO
-import csv
-
 
 # Clear file
 with open('_data/_tracks/tracks.csv', mode='w', newline='') as file:
     file.write('')
 
 # Config
-model = YOLO("_models/200SGD32-2/best.pt")  # load a pretrained model
+model = YOLO("_models/200Adam64-4/best.pt")  # load a pretrained model
 cfg_tracker = "_cfg/botsort.yaml"
 source_camera = 0
-source_video = "_data/_video/test_video3.mp4" #predict_and_track
-#source_video = "_data/_backup/predict_and_track.mp4" #predict_and_track
+#source_video = "_data/_video/test_video3.mp4" #predict_and_track
+source_video = "_data/_backup/street_view2.mp4" #predict_and_track
 
 # Tracking
 def track(mdl, tracker, source):
     mdl.track(source=source, show=True, tracker=tracker, line_thickness=2)
 
-
 # Track either camera or saved video
 #camera = track(model, cfg_tracker, source_camera)
-video = track(model, cfg_tracker, source_video)
+#video = track(model, cfg_tracker, source_video)
 
 
