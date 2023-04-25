01c01b02e30e765140ae81db3becf2a6117f462d
nirtus <nirtulys@gmail.com>
2023-04-25 22:07:11 +0300

    calculating speed in sector 1

--- det_plot_file copy.py
+++ det_plot_file copy.py
@@ -1,12 +1,14 @@
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+from shapely.geometry import Point, Polygon
 
 # Data parameteres
-everynth = 3 # show every other point (f.e. 3 - show every 3rd point)
+everynth = 1 # show every other point (f.e. 3 - show every 3rd point)
 start = 0 # start point
 end = -1 # end point
+objectId = 12 # all objects. For specific object, change 0 to objectid 
 
 # Prerecorded tracking results for ploting
 track_files = [
@@ -45,10 +47,19 @@ def tracks(file, everyth, start, end):
             if len(row) == 0 or index % everyth != 0:  # skip empty rows and select every other member
                 continue
             else:
+                # Sometimes several objects are detected during same time tick. For plotting, we need flatten dimmensions
                 for line in row:
-                    plot_tracks.append(track_format(line))
-                    print(track_format(line))
-            
+                    fline = track_format(line)
+                    
+                    if objectId == 0: # All objects
+                        plot_tracks.append(fline)
+                    else:
+                        if fline[0] == objectId: # Single object by id
+                            plot_tracks.append(fline)
+
+    #for line in plot_tracks:
+        #print(line) 
+
     return plot_tracks
 
 # Define a color mapping function
@@ -77,6 +88,48 @@ def scatter_3d(df, track_file):
 
 def scatter(df, x, y, track_file):
     img = Image.open('_data/_images/scene.png')
+    draw = ImageDraw.Draw(img)
+    width, height = img.size
+
+    # Draw sector for object speed calculations
+    quadrilateral = [(579, 449), (510, 407), (635, 372), (713, 414)]
+    draw.polygon(quadrilateral, fill="grey", outline="red")
+
+    # Create a Polygon object from the quadrilateral coordinates
+    polygon = Polygon(quadrilateral)
+
+    # Check if each point in the DataFrame is inside the sector
+    sp = []
+    for index, row in df.iterrows():
+        point = Point(row["x"], row["y"])
+        if polygon.contains(point):
+            sp.append([int(row['x']), int(row['y']), row['time']])
+
+    # Calculate speed
+    distance = 4 # Distance is taken based on maps.lt
+    time = round(sp[-1][2] - sp[0][2], 2)
+    speed = round(distance/time * 3.6, 2)
+    print(speed, "km/h")
+
+    # Set the font and size
+    font = ImageFont.truetype("arial.ttf", 20)
+    # Set the text to be drawn
+    text = f"{str(speed)} km/h"
+    # Draw the text on the image
+    draw.text((560, 400), text, font=font)
+
+    # # Draw start and end sectors
+    # start_point_1 = (579, 449)
+    # start_point_2 = (713, 414)
+
+    # end_point_1 = (510, 407)
+    # end_point_2 = (635, 372)
+
+    # draw.line((start_point_1, start_point_2), fill=(255, 255, 0), width=4)
+    # draw.line((end_point_1, end_point_2), fill=(255, 255, 0), width=4)
+    # # EOF Draw start and end sectors
+
+
     # Calculate the new size based on the percentage
     percent = 100
     width, height = img.size
@@ -106,7 +159,7 @@ def draw_plots(track_file):
         columns = ["id", "type", "x", "y", "time"]
     )
 
-    scatter_3d(df, track_file)
+    #scatter_3d(df, track_file)
     scatter(df, 'x', 'y', track_file)
 
     plt.show()
